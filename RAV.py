cee052eda11b555910c197e633200f39cf61a03f
ritmatter <mritter123@gmail.com>
2015-03-10 01:55:19 -0400

    Add css bootstrap and jquery

--- RAV.py
+++ RAV.py
@@ -3,8 +3,10 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 from werkzeug.utils import secure_filename
 import mixer
 import input_handler as ih
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = '\x05\xa3\xdfu\xad\xd9\x7f|\r\x12\xa5S\x18)0\x8cE\xe7\xd8?\x81\xe4\x1dC'
 UPLOAD_FOLDER = 'uploads/'
 MIX_FOLDER = 'mixes/'
@@ -14,42 +16,46 @@ app.config['MIX_FOLDER'] = MIX_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
+    # If a post was received, process the arguments
+    if request.method == 'POST':
+        file = request.files['file']
+        base_shard_length = request.form['minlength']
+        max_shard_length = request.form['maxlength']
+        total_time = request.form['total']
+        clip_name = request.form['clipname']
+        try:
+          bsl, msl, total = ih.check_inputs(file, base_shard_length, max_shard_length, total_time)
+        except ih.Error as e:
+          print e.msg
+          flash(e.msg)
+          return redirect(url_for('index'))
 
-		file = request.files['file']
-		base_shard_length = request.form['minimum_shard_length']
-		max_shard_length = request.form['maximum_shard_length']
-		total_time = request.form['total_time']
-		clip_name = request.form['clip_name']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filename = ih.convert_to_wav(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file = open(filename, 'r')
 
-		try:
-			bsl, msl, total = ih.check_inputs(file, base_shard_length, max_shard_length, total_time)
-		except ih.Error as e:
-			flash(e.msg)
-			return redirect(url_for('index'))
+        mixer.mix(file, bsl, msl, total, clip_name)
+        return redirect(url_for('mixed_file', filename=clip_name + ".mp3"))
 
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		filename = ih.convert_to_wav(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		file = open(filename, 'r')
+    # If we had a get request, simply return the form
+    return render_template('index.html')
 
-		mixer.mix(file, bsl, msl, total, clip_name)
-		return redirect(url_for('mixed_file', filename=clip_name + ".mp3"))
-
-	# If we had a get request, return the form
-	return render_template('index.html')
-
+# Get a file from mixes
 @app.route('/mixes/<filename>')
 def mixed_file(filename):
-	return send_from_directory(app.config['MIX_FOLDER'], filename)
+	  return send_from_directory(app.config['MIX_FOLDER'], filename)
 
+# Get a file from uploads
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# The error handler page
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
+# Run the app
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True, port=8080)
--- input_handler.py
+++ input_handler.py
@@ -6,6 +6,7 @@ INVALID_FILE = "Oops! Something was wrong with your input file. Make sure the fi
 INVALID_ARGUMENTS = "Oops! Something was wrong with your specifications. Make sure the base shard length and maximum shard length are both valid numbers and neither are longer than the input file length."
 ALLOWED_EXTENSIONS = set(['mp3', 'wav'])
 
+# Convert a file from wav to mp3
 def convert_to_wav(filename):
 	if is_wav(filename):
 		return filename
@@ -14,6 +15,7 @@ def convert_to_wav(filename):
 	AudioSegment.from_mp3(filename).export(path, format="wav")
 	return path
 
+# Convert a file from wav to mp3
 def convert_to_mp3(filename):
 	if is_mp3(filename):
 		return filename
@@ -22,12 +24,18 @@ def convert_to_mp3(filename):
 	AudioSegment.from_wav(filename).export(path, format="mp3")
 	return path
 
+# Determine if a file is an mp3
 def is_mp3(filename):
 	return filename.rsplit('.', 1)[1] == "mp3"
 
+# Determine if a file is a wav
 def is_wav(filename):
-	return filename.rsplit('.', 1)[1] == "wav" 
+	return filename.rsplit('.', 1)[1] == "wav"
 
+# Check the inputs of a file
+# file: the original file
+# bsl: minimum shard length
+# msl: maximum shard length
 def check_inputs(file, bsl, msl, total):
 	if not (file and allowed_file(file.filename)):
 		raise Error(INVALID_FILE)
@@ -39,7 +47,6 @@ def check_inputs(file, bsl, msl, total):
 		raise Error(INVALID_ARGUMENTS)
 
 	if bsl > total or msl > total:
-		print "error with total"
 		raise Error(INVALID_ARGUMENTS)
 
 	if bsl > msl:
@@ -49,6 +56,7 @@ def check_inputs(file, bsl, msl, total):
 
 	return bsl, msl, total
 
+# Determine if a file is allowed
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
